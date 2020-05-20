776bc6c6dd7d703f495b880822fce4a43c2f6203
dansbrainy <47524893+dansbrainy@users.noreply.github.com>
2020-05-20 17:44:10 +0200

    Login and Registration UI

---
+++ src/Services/accounts.js
@@ -0,0 +1,9 @@
+// import app from '../app';
+// const startApp = async () => {
+//   const header = document.querySelector('[data-app-name]');
+//   if (!header) return;
+//   const programName = await app();
+//   header.textContent = programName;}
+// function signup() {
+//   document.getElementById('switchSignin').style.cssText = 'display: none';
+// }
